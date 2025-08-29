e28eef85e6df89ee5c6d951186de6cbdc6cba7ce
zahar <znaumets@gmail.com>
2025-08-29 19:25:48 +0300

    Small fix

--- verifier/crates/storage/src/keys.rs
+++ verifier/crates/storage/src/keys.rs
@@ -2,27 +2,28 @@ use crate::{models::Key, traits::KeyStorage};
 use persistent_storage::init::PostgresRepo;
 use crate::errors::DatabaseError;
 use sqlx;
+use uuid::Uuid;
 
 impl KeyStorage for PostgresRepo {
-    async fn get_key(&self, key_id: &str) -> Result<Key, DatabaseError> {
+    async fn get_key(&self, key_id: &Uuid) -> Result<Key, DatabaseError> {
         let key = sqlx::query_as!(
             Key,
             "SELECT key_id, metadata FROM keys WHERE key_id = $1",
             key_id
         )
-        .fetch_one(self.pool)
+        .fetch_one(&self.pool)
         .await.map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
 
         Ok(key)
     }
 
     async fn create_key(&self, key: &Key) -> Result<(), DatabaseError> {
-        let key = sqlx::query!(
+        let _ = sqlx::query!(
             "INSERT INTO keys (key_id, metadata) VALUES ($1, $2) RETURNING key_id, metadata",
             key.key_id,
             key.metadata
         )
-        .fetch_one(self.pool)
+        .fetch_one(&self.pool)
         .await.map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
 
         Ok(())
--- verifier/crates/storage/src/traits.rs
+++ verifier/crates/storage/src/traits.rs
@@ -1,7 +1,8 @@
 use persistent_storage::init::{PostgresRepo, PersistentRepoTrait};
 use crate::{models::Key, errors::DatabaseError};
+use uuid::Uuid;
 
 pub trait KeyStorage {
-    async fn get_key(&self, key_id: &str) -> Result<Key, DatabaseError>;
+    async fn get_key(&self, key_id: &Uuid) -> Result<Key, DatabaseError>;
     async fn create_key(&self, key: &Key) -> Result<(), DatabaseError>;
 }
