5419c12b34aa4ec64b95bf83882279234d738418
zahar <znaumets@gmail.com>
2025-08-30 10:10:21 +0300

    Fix verifier queries

--- verifier/crates/storage/src/keys.rs
+++ verifier/crates/storage/src/keys.rs
@@ -1,30 +1,27 @@
 use crate::{models::Key, traits::KeyStorage};
 use persistent_storage::init::PostgresRepo;
 use crate::errors::DatabaseError;
-use sqlx;
+use sqlx::{self, Row};
 use uuid::Uuid;
 
 impl KeyStorage for PostgresRepo {
     async fn get_key(&self, key_id: &Uuid) -> Result<Key, DatabaseError> {
-        let key = sqlx::query_as!(
-            Key,
-            "SELECT key_id, metadata FROM keys WHERE key_id = $1",
-            key_id
-        )
-        .fetch_one(&self.pool)
-        .await.map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
+        let result: (Uuid, String) = sqlx::query_as("SELECT key_id, metadata FROM keys WHERE key_id = $1")
+            .bind(key_id)
+            .fetch_one(&self.pool)
+            .await
+            .map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
 
-        Ok(key)
+        Ok(Key { key_id: result.0, metadata: result.1 })
     }
 
     async fn create_key(&self, key: &Key) -> Result<(), DatabaseError> {
-        let _ = sqlx::query!(
-            "INSERT INTO keys (key_id, metadata) VALUES ($1, $2) RETURNING key_id, metadata",
-            key.key_id,
-            key.metadata
-        )
-        .fetch_one(&self.pool)
-        .await.map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
+        let _ = sqlx::query("INSERT INTO keys (key_id, metadata) VALUES ($1, $2) RETURNING key_id, metadata")
+            .bind(key.key_id)
+            .bind(key.metadata.clone())
+            .execute(&self.pool)
+            .await
+            .map_err(|e| DatabaseError::BadRequest(e.to_string()))?;
 
         Ok(())
     }
