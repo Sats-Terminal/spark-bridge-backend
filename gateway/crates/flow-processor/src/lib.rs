6b66da7766b83d49be74ee5e346a9f124358c54f
zahar <znaumets@gmail.com>
2025-09-01 13:13:57 +0300

    move magic number to struct field

--- gateway/crates/flow-processor/src/lib.rs
+++ gateway/crates/flow-processor/src/lib.rs
@@ -11,10 +11,13 @@ use persistent_storage::init::PostgresRepo;
 use tokio::sync::mpsc;
 use tokio_util::sync::CancellationToken;
 
-pub fn create_flow_processor(storage: PostgresRepo) -> (FlowProcessor, FlowSender) {
+pub fn create_flow_processor(
+    storage: PostgresRepo,
+    cancellation_retries: u64,
+) -> (FlowProcessor, FlowSender) {
     let (tx_sender, tx_receiver) = mpsc::channel(1000);
     let cancellation_token = CancellationToken::new();
-    let flow_processor = FlowProcessor::new(tx_receiver, storage, cancellation_token.clone());
+    let flow_processor = FlowProcessor::new(tx_receiver, storage, cancellation_token.clone(), cancellation_retries);
     let flow_sender = FlowSender::new(tx_sender, cancellation_token);
     (flow_processor, flow_sender)
 }
