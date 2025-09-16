56419266406458eaf16fdbfa3fa9d6f2a3e40234
zahar <znaumets@gmail.com>
2025-09-16 18:36:12 +0300

    Add btc indexer client

---
+++ verifier/crates/btc_indexer_client/src/client.rs
@@ -0,0 +1,38 @@
+use reqwest::Client;
+use serde::{Serialize, Deserialize};
+use crate::error::BtcIndexerClientError;
+use verifier_config_parser::config::BtcIndexerConfig;
+use url::Url;
+
+const WATCH_RUNES_DEPOSIT_PATH: &str = "/track_tx";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WatchRunesDepositRequest {
+    pub tx_id: String,
+    pub callback_url: Url,
+}
+
+#[derive(Clone, Debug)]
+pub struct BtcIndexerClient {
+    config: BtcIndexerConfig,
+    client: Client,
+}
+
+impl BtcIndexerClient {
+    pub fn new(config: BtcIndexerConfig) -> Self {
+        Self { config, client: Client::new() }
+    }
+
+    pub async fn watch_runes_deposit(&self, request: WatchRunesDepositRequest) -> Result<(), BtcIndexerClientError> {
+        let url = self.config.address.join(WATCH_RUNES_DEPOSIT_PATH)
+            .map_err(|e| BtcIndexerClientError::DeserializeError(format!("Failed to join URL: {:?}", e)))?;
+        let response = self.client.post(url).json(&request).send().await
+            .map_err(|e| BtcIndexerClientError::HttpError(format!("Failed to send request: {:?}", e)))?;
+
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            Err(BtcIndexerClientError::HttpError(format!("Failed to send HTTP request with status {}, error: {}", response.status(), response.text().await.unwrap_or_default())))
+        }
+    }
+}
