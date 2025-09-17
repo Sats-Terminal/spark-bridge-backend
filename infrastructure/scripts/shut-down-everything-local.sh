dbbe3ac2595c09ff51291c43f7a284c02dfeee48
zahar <znaumets@gmail.com>
2025-09-17 14:43:27 +0300

    quick fix

--- infrastructure/scripts/shut-down-everything-local.sh
+++ infrastructure/scripts/shut-down-everything-local.sh
@@ -12,13 +12,17 @@ VERIFIER_2_DATABASE_URL="postgres://postgres:postgres@localhost:5472/verifier"
 VERIFIER_3_DATABASE_URL="postgres://postgres:postgres@localhost:5473/verifier"
 BTC_INDEXER_DATABASE_URL="postgres://postgres:postgres@localhost:5474/btc_indexer"
 
+GATEWAY_MIGRATION_PATH="./gateway/crates/local_db_store/migrations"
+VERIFIER_MIGRATION_PATH="./verifier/crates/local_db_store/migrations"
+BTC_INDEXER_MIGRATION_PATH="./btc_indexer/crates/local_db_store/migrations"
+
 revert_migrations() {
     echo "Reverting migrations..."
-    sqlx migrate revert --database-url $GATEWAY_DATABASE_URL --source ./gateway/crates/local_db_store/migrations
-    sqlx migrate revert --database-url $VERIFIER_1_DATABASE_URL --source ./verifier/crates/local_db_store/migrations
-    sqlx migrate revert --database-url $VERIFIER_2_DATABASE_URL --source ./verifier/crates/local_db_store/migrations
-    sqlx migrate revert --database-url $VERIFIER_3_DATABASE_URL --source ./verifier/crates/local_db_store/migrations
-    sqlx migrate revert --database-url $BTC_INDEXER_DATABASE_URL --source ./btc_indexer/crates/local_db_store/migrations
+    sqlx migrate revert --database-url $GATEWAY_DATABASE_URL --source $GATEWAY_MIGRATION_PATH
+    sqlx migrate revert --database-url $VERIFIER_1_DATABASE_URL --source $VERIFIER_MIGRATION_PATH
+    sqlx migrate revert --database-url $VERIFIER_2_DATABASE_URL --source $VERIFIER_MIGRATION_PATH
+    sqlx migrate revert --database-url $VERIFIER_3_DATABASE_URL --source $VERIFIER_MIGRATION_PATH
+    sqlx migrate revert --database-url $BTC_INDEXER_DATABASE_URL --source $BTC_INDEXER_MIGRATION_PATH
     echo "Migrations reverted successfully."
 }
 
