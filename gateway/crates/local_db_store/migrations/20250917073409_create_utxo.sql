ab18e278397fe6131373eecc74eebf28de3ca69e
zahar <znaumets@gmail.com>
2025-09-19 08:16:31 +0300

    Fix configs

--- gateway/crates/local_db_store/migrations/20250917073409_create_utxo.sql
+++ gateway/crates/local_db_store/migrations/20250917073409_create_utxo.sql
@@ -6,13 +6,12 @@ CREATE TYPE UTXO_STATUS AS ENUM (
 
 CREATE TABLE IF NOT EXISTS gateway.utxo
 (
-    out_point    TEXT        NOT NULL,
+    out_point    TEXT        PRIMARY KEY,
     amount       BIGINT      NOT NULL,
     rune_id      TEXT        NOT NULL,
     sats_fee_amount BIGINT   ,
     status       UTXO_STATUS NOT NULL DEFAULT 'pending',
-    btc_address  TEXT        NOT NULL,
-    PRIMARY KEY (txid, vout)
+    btc_address  TEXT        NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_utxo_status ON gateway.utxo (status);
