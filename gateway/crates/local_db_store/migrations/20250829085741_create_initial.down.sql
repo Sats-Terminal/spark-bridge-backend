ae1b7656b8350528c7794d5d1086eeff44272a66
ikripaka <kripakaillya@gmail.com>
2025-09-26 10:28:26 +0300

    change gateway migrations to align with new dkg pregen api

--- gateway/crates/local_db_store/migrations/20250829085741_create_initial.down.sql
+++ gateway/crates/local_db_store/migrations/20250829085741_create_initial.down.sql
@@ -1,8 +1,9 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS gateway.sign_session;
 DROP TABLE IF EXISTS gateway.deposit_address;
-DROP TABLE IF EXISTS gateway.musig_identifier;
+DROP TABLE IF EXISTS gateway.sign_session;
+DROP TABLE IF EXISTS gateway.user_identifier;
+DROP TABLE IF EXISTS gateway.dkg_share;
 DROP TABLE IF EXISTS gateway.utxo;
 DROP TABLE IF EXISTS gateway.session_requests;
 
--- gateway/crates/local_db_store/migrations/20250829085741_create_initial.up.sql
+++ gateway/crates/local_db_store/migrations/20250829085741_create_initial.up.sql
@@ -4,22 +4,22 @@ CREATE SCHEMA gateway;
 
 ----------- MUSIG_IDENTIFIER -----------
 
-CREATE TABLE IF NOT EXISTS gateway.musig_identifier
+CREATE TABLE IF NOT EXISTS gateway.user_identifier
 (
-    public_key TEXT    NOT NULL,
-    rune_id    TEXT    NOT NULL,
-    is_issuer  BOOLEAN NOT NULL,
-    dkg_state  JSON    NOT NULL,
-    PRIMARY KEY (public_key, rune_id)
+    user_uuid    UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    dkg_share_id UUID             NOT NULL,
+    public_key   TEXT             NOT NULL,
+    rune_id      TEXT             NOT NULL,
+    is_issuer    BOOLEAN          NOT NULL,
+    PRIMARY KEY (user_uuid),
+    FOREIGN KEY (dkg_share_id) REFERENCES gateway.dkg_share (dkg_share_id)
 );
 
 -- Dkg pregenerated shares
 CREATE TABLE IF NOT EXISTS gateway.dkg_share
 (
-    id         SERIAL PRIMARY KEY NOT NULL,
---     user pubkey from `musig_id` that is stuck to this entry
-    public_key TEXT,
-    dkg_state  JSONB              NOT NULL
+    dkg_share_id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    dkg_state    JSONB            NOT NULL
 );
 
 ----------- SIGN_SESSION -----------
@@ -27,14 +27,13 @@ CREATE TABLE IF NOT EXISTS gateway.dkg_share
 CREATE TABLE IF NOT EXISTS gateway.sign_session
 (
     session_id   TEXT  NOT NULL,
-    public_key   TEXT  NOT NULL,
-    rune_id      TEXT  NOT NULL,
+    user_uuid    UUID  NOT NULL,
     tweak        BYTEA,
     message_hash BYTEA NOT NULL,
     metadata     JSON  NOT NULL,
     sign_state   JSON  NOT NULL,
     PRIMARY KEY (session_id),
-    FOREIGN KEY (public_key, rune_id) REFERENCES gateway.musig_identifier (public_key, rune_id)
+    FOREIGN KEY (user_uuid) REFERENCES gateway.user_identifier (user_uuid)
 );
 
 ------------ DEPOSIT_ADDRESS -----------
@@ -42,15 +41,14 @@ CREATE TABLE IF NOT EXISTS gateway.sign_session
 CREATE TABLE IF NOT EXISTS gateway.deposit_address
 (
     nonce_tweak         BYTEA   NOT NULL,
-    public_key          TEXT    NOT NULL,
-    rune_id             TEXT    NOT NULL,
+    user_uuid           UUID    NOT NULL,
     deposit_address     TEXT    NOT NULL,
     bridge_address      TEXT,
     is_btc              BOOLEAN NOT NULL,
     amount              BIGINT  NOT NULL,
     confirmation_status JSON    NOT NULL,
-    PRIMARY KEY (public_key, rune_id, nonce_tweak),
-    FOREIGN KEY (public_key, rune_id) REFERENCES gateway.musig_identifier (public_key, rune_id)
+    PRIMARY KEY (user_uuid, nonce_tweak),
+    FOREIGN KEY (user_uuid) REFERENCES gateway.user_identifier (user_uuid)
 );
 
 ------------ UTXO -----------
