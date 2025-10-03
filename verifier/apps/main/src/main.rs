7b0802215d417d09a0c5ca95da39f656aac1a18e
Andezion <245122@edu.p.lodz.pl>
2025-10-03 02:16:21 +0200

    checked main verifier

--- verifier/apps/main/src/main.rs
+++ verifier/apps/main/src/main.rs
@@ -14,12 +14,10 @@ async fn main() {
     let _ = dotenv::dotenv();
     let _logger_guard = init_logger();
 
-    // Create Config
     let config_path = ConfigPath::from_env().unwrap();
     let server_config = ServerConfig::init_config(config_path.path);
     tracing::debug!("App config: {:?}", server_config);
 
-    // Create DB Pool
     let postgres_creds = PostgresDbCredentials {
         url: server_config.database.url.clone(),
     };
@@ -27,7 +25,6 @@ async fn main() {
         postgres_repo: PostgresRepo::from_config(postgres_creds).await.unwrap(),
     });
 
-    // Create Frost Signer
     let frost_signer = FrostSigner::new(
         server_config.frost_signer.identifier,
         storage.clone(),
@@ -36,7 +33,6 @@ async fn main() {
         server_config.frost_signer.threshold,
     );
 
-    // Create App
     let app = verifier_server::init::create_app(
         frost_signer,
         server_config.btc_indexer.clone(),
@@ -47,7 +43,6 @@ async fn main() {
     )
     .await;
 
-    // Run App
     let addr_to_listen = format!("{}:{}", server_config.server.ip, server_config.server.port);
     let listener = TcpListener::bind(addr_to_listen).await.unwrap();
     axum::serve(listener, app).await.unwrap();
